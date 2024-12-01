6c5a615ff46b8b48fdbaa686a878c4f849dc0aae
yuta15 <yxyi8udm1eb1us6aea7s@gmail.com>
2024-12-02 01:54:19 +0900

    conftest.pyへtable作成用コードを追記

--- app/tests/unit/conftest.py
+++ app/tests/unit/conftest.py
@@ -1,4 +1,6 @@
 import pytest
+
+from app.tests.mods.get_db import create_table
 from app.tests.mods.FakeUser import FakeUser
 
 
@@ -7,6 +9,7 @@ def user_fixture():
     """
     疑似的なUserデータを生成するための関数
     """
+    create_table()
     def _generate_user(user_params):
         return FakeUser(user_params)
         
