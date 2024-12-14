e128b5bb50a913944936480761ea041e89fa708c
yuta15 <yxyi8udm1eb1us6aea7s@gmail.com>
2024-12-14 21:14:21 +0900

    各種関数の修正

--- docker/db/initdb.d/0_create_table.sql
+++ docker/db/initdb.d/0_create_table.sql
@@ -1,11 +1,12 @@
 CREATE TABLE IF NOT EXISTS main.user(
+	`uuid` CHAR(32) NOT NULL, 
 	`username` VARCHAR(50) NOT NULL, 
 	`email` VARCHAR(255) NOT NULL, 
-	`uuid` CHAR(32) NOT NULL, 
 	`create_at` DATETIME NOT NULL, 
 	`update_at` DATETIME NOT NULL, 
 	`hashed_password` VARCHAR(255) NOT NULL, 
 	`is_active` BOOL NOT NULL, 
+	`is_admin` BOOL NOT NULL,
 	PRIMARY KEY (uuid)
 );
 CREATE UNIQUE INDEX ix_user_uuid ON user(uuid);
@@ -13,25 +14,10 @@ CREATE UNIQUE INDEX ix_user_username ON user(username);
 CREATE UNIQUE INDEX ix_user_email ON user(email);
 
 
-CREATE TABLE superuser (
-	`username` VARCHAR(50) NOT NULL, 
-	`email` VARCHAR(255) NOT NULL, 
-	`uuid` CHAR(32) NOT NULL, 
-	`create_at` DATETIME NOT NULL, 
-	`update_at` DATETIME NOT NULL, 
-	`hashed_password` VARCHAR(255) NOT NULL, 
-	`is_active` BOOL NOT NULL, 
-	PRIMARY KEY (uuid)
-);
-CREATE UNIQUE INDEX ix_superuser_uuid ON superuser(uuid);
-CREATE UNIQUE INDEX ix_superuser_username ON superuser(username);
-CREATE UNIQUE INDEX ix_superuser_email ON superuser(email);
-
-
 CREATE TABLE article (
+	`id` CHAR(32) NOT NULL, 
 	`title` VARCHAR(150) NOT NULL, 
 	`body` LONGTEXT, 
-	`id` CHAR(32) NOT NULL, 
 	`creaeted_at` DATETIME NOT NULL, 
 	`updated_at` DATETIME NOT NULL, 
 	`is_public` BOOL NOT NULL, 
