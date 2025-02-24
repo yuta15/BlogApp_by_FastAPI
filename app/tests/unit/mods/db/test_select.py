48ff9f1986a341935d13ead9bdf3e05ec67b2d8a
yuta15 <yxyi8udm1eb1us6aea7s@gmail.com>
2025-02-24 16:44:22 +0900

    test_select_articles関数を新規作成.articleにおいてもselectが実行可能であることを確認

--- app/tests/unit/mods/db/test_select.py
+++ app/tests/unit/mods/db/test_select.py
@@ -3,6 +3,7 @@ from sqlmodel import select
 
 from app.mods.db.select import select_data
 from app.models.User import User
+from app.models.Article import Article
 
 
 
@@ -16,3 +17,15 @@ def test_select_user(db_session, create_users, insert_data_fixture):
         assert user == select_data(session=session, stmts=select(User).where(User.username == user.username))[0]
         assert user == select_data(session=session, stmts=select(User).where(User.email == user.email))[0]
         assert user == select_data(session=session, stmts=select(User).where(User.uuid == user.uuid))[0]
+
+
+def test_select_articles(db_session, create_users, create_articles, insert_data_fixture):
+    """select_dataのテスト"""
+    user_count = 4
+    session = db_session
+    users = create_users(number=user_count)
+    for user in users:
+        insert_data_fixture(session=session, data=user)
+        article = create_articles(user_id=user.uuid)[0]
+        insert_data_fixture(session=session, data=article)
+        assert article == select_data(session=session, stmts=select(Article).where(Article.id == article.id))[0]
